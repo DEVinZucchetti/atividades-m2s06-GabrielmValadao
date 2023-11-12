32e55204f476cbbd9bab48626aff7e8552ab3de6
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-11-12 20:35:25 -0300

    feat: cria recurso post para salvar pessoa

--- atividade-semana-06/app/Http/Controllers/PessoasController.php
+++ atividade-semana-06/app/Http/Controllers/PessoasController.php
@@ -3,17 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Pessoa; 
+use App\Models\Pessoa;
 
 class PessoasController extends Controller
+
+// pega o dado cadastrado
 {
-    public function index() {
+    public function index()
+    {
         try {
-            $pessoas = Pessoa::all();   
-            $message = $pessoas->count()." ". ($pessoas->count() === 1 ? "pessoa encontrada" : "pessoas encontradas")." com sucesso.";
+            $pessoas = Pessoa::all();
+            $message = $pessoas->count() . " " . ($pessoas->count() === 1 ? "pessoa encontrada" : "pessoas encontradas") . " com sucesso.";
             return $this->response($message, $pessoas);
         } catch (\Exception $exception) {
-            return $this->response($exception->getMessage(), null, false, 500);   
+            return $this->response($exception->getMessage(), null, false, 500);
+        }
+    }
+
+    // salva o dado cadastrado 
+    public function store(Request $request)
+    {
+        try {
+
+            $request->validate([
+                'name' => 'required | min: 3 | max: 150',
+                'cpf' => 'min: 11 | max: 20',
+                'contact' => 'max: 20',
+            ]);
+
+            $pessoa = Pessoa::create($request->all());
+            $message = $pessoa->name . "cadastrado com sucesso";
+            return $this->response($message, $pessoa);
+        } catch (\Exception $exception) {
+            return $this->response($exception->getMessage(), null, false, 500);
         }
     }
 }
