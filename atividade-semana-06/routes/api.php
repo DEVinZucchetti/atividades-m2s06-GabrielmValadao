ca012b768ab9d14ebaf04efb5ebbd875dccf64e2
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-11-13 22:14:38 -0300

    update: atualiza sintaxe da route de api.php

--- atividade-semana-06/routes/api.php
+++ atividade-semana-06/routes/api.php
@@ -4,23 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PessoasController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::prefix('pessoas')->group(function() {
-    Route::get('', [PessoasController::class, 'index']);
-    Route::post('', [PessoasController::class, 'store']);
-    Route::put('{id}', [PessoasController::class, 'update']);
-    Route::delete('{id}', [PessoasController::class, 'delete']);
-});
+Route::resource('pessoas', PessoasController::class)
+  ->only(['index', 'show', 'store', 'update', 'destroy']);
 
 
 /*
